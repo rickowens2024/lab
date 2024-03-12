d15d8f3ea8b8f75c8e87862fb1ba16220015fd05
stanislavverevkin <stanislav.verovkin@werush.co>
2024-03-12 10:17:33 +0200

    init v2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "task1/Task1.h"
-#include "task2/Task2.h"
-#include "Task3/Task3.h"
-#include "task4/Task4.h"
-#include "task5/Task5.h"
+#include "Task1.h"
+#include "Task2.h"
+#include "Task3.h"
+#include "Task4.h"
+#include "Task5.h"
 
 int main() {
     Task1();
